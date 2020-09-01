eb3be1e0a5cc219c9f4cf4be1821ecc4533be251
mherling <61545864+mherling@users.noreply.github.com>
2020-09-01 10:57:26 +0200

    new properties and methods

--- Simulation.m
+++ Simulation.m
@@ -1,12 +1,25 @@
 classdef Simulation
     properties
-        simulationModules
+        simulationModules = []
+        startDate datetime
+        endDate datetime
+        timeStep int32
     end
     methods
+        function registerSimulationModule(moduleClass)
+            obj.simulationModules(1,obj.getModulesCount()+1) = moduleClass;
+        end
+
+        function n = getModulesCount()
+            n = size(obj.simulationModules)
+        end
+
         function reset()
-            
           
         end
+        
         function r = run()
+
+        end
     end
 end
