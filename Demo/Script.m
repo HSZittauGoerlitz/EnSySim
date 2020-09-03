3f93085469e81426ff12fa7c23ba246acea2fb3c
mherling <61545864+mherling@users.noreply.github.com>
2020-09-03 12:06:57 +0200

    ignore autosave (*.asv)

--- Demo/Script.m
+++ Demo/Script.m
@@ -3,10 +3,6 @@ sim = Simulator();
 esim = sim.registerSimulator(ElectricalSimulator)
 asim = sim.registerSimulator(AgentSimulator)
 
-% define simulation times
-sim.timeStep = 60
-sim.startDate = datetime(2020,1,1,0,0,0)
-sim.endDate = datetime(2020,1,2,0,0,0)
 
 % add agents
 for each = COCarray
@@ -15,5 +11,10 @@ for each = COCarray
     asim.add(agent)
 end
 
+
+% define simulation times
+sim.timeStep = 60
+sim.startDate = datetime(2020,1,1,0,0,0)
+sim.endDate = datetime(2020,1,2,0,0,0)
 
 sim.run()
