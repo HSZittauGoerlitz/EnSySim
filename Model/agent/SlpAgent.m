47af2d597e1eb30669e61b4bf42545f440c56db1
mherling <61545864+mherling@users.noreply.github.com>
2020-09-09 10:00:53 +0200

    Update SlpAgent
    needs to implement methods calculate, update

--- Model/agent/SlpAgent.m
+++ Model/agent/SlpAgent.m
@@ -1,3 +1,27 @@
 classdef SlpAgent < AbstractSimulationAgent
+    %% Agents with loads based on standard load profiles
+    % for generation of profiles see
+    % Model.agent.AgentSimulator.createLoadProfiles
+    % agents will have 
+    properties
+        % type of agent
+        agentType
+        % COC value used to scale loads
+        coc
+
+        
+    end
+    
+    methods
+        function calculate(obj, time, timeStep)
+            % calculate next time step load
+            % obj.internalDeltaEnergy = load * timeStep
+        end
+        
+        function update(obj)
+            % write resulting load to 
+        end
+           
+    end
     
 end
