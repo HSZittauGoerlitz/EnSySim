44681feb265a3155540559457626aaa089d86818
mherling <61545864+mherling@users.noreply.github.com>
2020-09-11 15:38:45 +0200

    Create electrical Slp element
    generic electrical consumer based on slp

---
+++ Model/electrical/ElectricalSlpSimulationElement.m
@@ -0,0 +1,13 @@
+classdef ElectricalSlpSimulationElement < ElectricalSimulatioElement
+
+
+    methods
+        function calculate(obj,args)
+            
+        end
+
+        function update(obj, args)
+            
+        end
+    end
+end
