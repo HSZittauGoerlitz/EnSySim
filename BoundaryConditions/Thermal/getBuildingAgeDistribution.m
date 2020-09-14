25c839fd11475b0271be134b25c68e862c36994d
SKittan <S.Kittan@hszg.de>
2020-09-14 11:52:45 +0200

    Change building Age categorie model to be based on original statistic probabilities

--- BoundaryConditions/Thermal/getBuildingAgeDistribution.m
+++ BoundaryConditions/Thermal/getBuildingAgeDistribution.m
@@ -1,30 +1,28 @@
 % import destatis data: https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Wohnen/Tabellen/wohneinheiten-nach-baujahr.html
 % building age distribution in saxony
-buildingAge(1:1155) = 1;  % before 1948
-buildingAge(1156:1702) = 2;  % 1949 bis 1978
-buildingAge(1703:1951) = 3;  % 1979 bis 1990
-buildingAge(1952:2300) = 4;  % 1991 bis 2010
-buildingAge(2301:2332) = 5;  % 2011 and later
+buildingAgeCat(1:1155) = 1;  % before 1948
+buildingAgeCat(1156:1702) = 2;  % 1949 bis 1978
+buildingAgeCat(1703:1951) = 3;  % 1979 bis 1990
+buildingAgeCat(1952:2300) = 4;  % 1991 bis 2010
+buildingAgeCat(2301:2332) = 5;  % 2011 and later
 
-weibFit = fitdist(buildingAge', 'weibul');
+% probabilities of each category
+pAgeCat = [0.4952, 0.2346, 0.1068, 0.1497, 0.0137];
+% generate new Data
+nExamples = 50000;
+pNewData = rand([nExamples, 1]);
+NewData = zeros([nExamples, 1]);
+pRight = pAgeCat_1;
+NewData(pNewData < pRight) = 1;
+for cat = 2:4
+    pLeft = pRight;
+    pRight = pRight + pAgeCat(cat);
+    NewData(pNewData >= pLeft & pNewData < pRight) = cat;
+end
+pLeft = pRight;
+NewData(pNewData >= pLeft) = cat+1;
 
-NewData = weibFit.random([50000, 1]);
-% split values smaller 1 to 1 or 5
-maskNewSmallerOne = NewData < 1;
-nNewSmallerOne = sum(maskNewSmallerOne);
-newValues = rand([nNewSmallerOne, 1]);
-maskNewValues = newValues <= 0.65;
-newValues(maskNewValues) = 1;
-newValues(~maskNewValues) = 5;
-NewData(maskNewSmallerOne) = newValues;
-% set data to int values
-NewData = floor(NewData);
-% set values greater than 5 to 5
-NewData(NewData > 5) = 5;
-% shift a small part of cat 2 - 4 to cat 5
-
-
-OrigPlot = histogram(buildingAge, 'Normalization', 'pdf', 'EdgeColor', [0.1 0.1 0.1], 'BinWidth', 1, ...
+OrigPlot = histogram(buildingAgeCat, 'Normalization', 'pdf', 'EdgeColor', [0.1 0.1 0.1], 'BinWidth', 1, ...
                      'DisplayStyle', 'stairs', 'DisplayName', 'original Data', 'LineWidth', 1);
 hold on
 NewPlot = histogram(NewData, 'Normalization', 'pdf', 'EdgeColor', [0.1 0.1 0.7], 'BinWidth', 1, ...
@@ -33,4 +31,4 @@ grid on
 xlabel('Building Category')
 ylabel('Probability')
 legend([OrigPlot, NewPlot])
-hold off
+hold off;
