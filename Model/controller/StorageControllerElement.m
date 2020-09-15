07dc7f4cf5bef39fb260209f1cb0d3fd8c38b684
mherling <61545864+mherling@users.noreply.github.com>
2020-09-15 10:04:08 +0200

    Create StorageControllerElement.m

---
+++ Model/controller/StorageControllerElement.m
@@ -0,0 +1,12 @@
+classdef StorageControllerElement < AbstractControllerElement
+
+    properties
+        
+    end
+
+    methods
+        function obj = StorageControllerElement(obj,args)
+            
+        end
+    end
+end
