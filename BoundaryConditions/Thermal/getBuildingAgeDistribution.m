7b19233b1787fba3d457af77dfb1f488bce004cc
SKittan <S.Kittan@hszg.de>
2020-09-15 08:29:30 +0200

    Add generation of modeled data set with year of construction

--- BoundaryConditions/Thermal/getBuildingAgeDistribution.m
+++ BoundaryConditions/Thermal/getBuildingAgeDistribution.m
@@ -1,6 +1,6 @@
 % import destatis data: https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Wohnen/Tabellen/wohneinheiten-nach-baujahr.html
 % building age distribution in saxony
-buildingAgeCat(1:1155) = 1;  % before 1948
+buildingAgeCat(1:1155) = 1;  % 1948 and before
 buildingAgeCat(1156:1702) = 2;  % 1949 bis 1978
 buildingAgeCat(1703:1951) = 3;  % 1979 bis 1990
 buildingAgeCat(1952:2300) = 4;  % 1991 bis 2010
@@ -32,3 +32,20 @@ xlabel('Building Category')
 ylabel('Probability')
 legend([OrigPlot, NewPlot])
 hold off;
+
+% generate building age data
+buildingAge = zeros([nExamples, 1]);
+% flatten the first cat by using also a year range
+buildingAge(NewData == 1) = randi([1920, 1948], ...
+                                  [sum(NewData == 1), 1]);
+buildingAge(NewData == 2) = randi([1949, 1978], ...
+                                  [sum(NewData == 2), 1]);
+buildingAge(NewData == 3) = randi([1979, 1990], ...
+                                  [sum(NewData == 3), 1]);
+buildingAge(NewData == 4) = randi([1991, 2010], ...
+                                  [sum(NewData == 4), 1]);
+buildingAge(NewData == 5) = randi([2010, 2018], ...
+                                  [sum(NewData == 5), 1]);
+                                     
+figure
+histogram(buildingAge, 'Normalization', 'pdf');
