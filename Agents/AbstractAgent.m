de82a68bea389b5c88d0a1e5f5b055eea05cfedc
SKittan <S.Kittan@hszg.de>
2020-09-18 11:17:29 +0200

    Add basic Agent structure

---
+++ Agents/AbstractAgent.m
@@ -0,0 +1,29 @@
+classdef (Abstract) AbstractAgent
+    %ABSTRACTAGENT Definition of the Basic Agent structure
+    
+    properties (Abstract)
+        % Common Parameter
+        timeStep
+        COCfactor
+        % Load
+        LoadProfile_e
+        LoadProfile_t
+        % Generation
+        Generation_e
+        Generation_t
+        % Storage
+        Storage_e
+        Storage_t
+        % Bilance
+        % resulting Load at given time step
+        % positive: Energy is consumed
+        % negative: Energy is generated
+        currentLoad_e  
+        cuurentLoad_t
+    end
+    
+    methods (Abstract)
+        update(self)
+    end
+end
+
