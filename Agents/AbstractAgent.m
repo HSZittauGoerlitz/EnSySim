cbb95ad1f28953aa91b7c6b66fa288e6b7ab89fa
SKittan <S.Kittan@hszg.de>
2020-09-18 14:16:43 +0200

    Fix spelling error

--- Agents/AbstractAgent.m
+++ Agents/AbstractAgent.m
@@ -18,7 +18,7 @@ classdef (Abstract) AbstractAgent
         % positive: Energy is consumed
         % negative: Energy is generated
         currentEnergyBilance_e  % [Wh]
-        cuurentEnergyBilance_t  % [Wh]
+        currentEnergyBilance_t  % [Wh]
     end
     
     methods (Abstract)
