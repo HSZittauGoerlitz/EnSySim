075428c908e0ea5210cd9d5d3bc5f4d8b941a551
SKittan <S.Kittan@hszg.de>
2020-09-21 08:35:55 +0200

    Add successful run with one agent

--- Tests/basicTest.m
+++ Tests/basicTest.m
@@ -7,3 +7,10 @@ time = startTime:minutes(15):endTime;
 normSLP = getNormSLPs(startTime, endTime);
 
 agent = PHHconsumer_e(normSLP, PHH_COC_distribution);
+
+%% Simulate
+idx = 0;
+for t = time
+    idx = idx + 1;
+    agent.update(idx);
+end
