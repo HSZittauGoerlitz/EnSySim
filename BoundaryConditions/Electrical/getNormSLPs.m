2cb1c25dc213ffbb67633058b3569ff0dcb69f42
SKittan <S.Kittan@hszg.de>
2020-09-29 07:08:24 +0200

    Move time input parsing to getTime method

--- BoundaryConditions/Electrical/getNormSLPs.m
+++ BoundaryConditions/Electrical/getNormSLPs.m
@@ -10,28 +10,7 @@ function SLP = getNormSLPs(startDate, endDate)
 %
 % Inputs:
 %   startDate - First date of SLP curve, complete day is considered (datetime)
-%   endDate   - Last date of SLP curve, complete day is considered (datetime)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Input paramter handling %
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%
-    p = inputParser;
-    addRequired(p, 'startDate', @isdatetime);
-    addRequired(p, 'endDate', @isdatetime);
-
-    % set first day clock to 0:00:00
-    startDate.Hour = 0;
-    startDate.Minute = 0;
-    startDate.Second = 0;
-    % set last day clock to 23:45:00
-    endDate.Hour = 23;
-    endDate.Minute = 45;
-    endDate.Second = 0;
-    
-    % Validate inputs
-    if startDate >= endDate
-       error("endDate must be after startDate"); 
-    end
-    
+%   endDate   - Last date of SLP curve, complete day is considered (datetime)  
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Prepare Time data and selection %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- BoundaryConditions/Weather/getGlobalRadiation.m
+++ BoundaryConditions/Weather/getGlobalRadiation.m
@@ -12,36 +12,19 @@ function globRad = getGlobalRadiation(startDate, endDate, regionProfile)
 %                   yearless time information (columns: doy, hour, minute).
 %                   Leap day data is stored in doy=0 rows, doy=1 is equal
 %                   to first january and doy=365 to last december of a year.
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Input paramter handling %
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%
-    p = inputParser;
-    addRequired(p, 'startDate', @isdatetime);
-    addRequired(p, 'endDate', @isdatetime);
-
-    % set first day clock to 0:00:00
-    startDate.Hour = 0;
-    startDate.Minute = 0;
-    startDate.Second = 0;
-    % set last day clock to 23:45:00
-    endDate.Hour = 23;
-    endDate.Minute = 45;
-    endDate.Second = 0;
-    
-    % Validate inputs
-    if startDate >= endDate
-       error("endDate must be after startDate"); 
-    end
-    
     %%%%%%%%%%%%%%%%%%%%%%%%%
     % Create glob rad curve %
     %%%%%%%%%%%%%%%%%%%%%%%%%
     leapDay = 60;  % save number of each leap day
     globRad.time = getTime(startDate, endDate);
+    globRad.Eg = zeros(height(globRad), 1);
     % Split up Eg data generation into linked doy sequences
+    doy = day(globRad.time, 'dayofyear');
     for year = min(globRad.time.Year):max(globRad.time.Year)
-        mask = Entsprechender Jahresabschnitt, beachten doy Korrektur, wenn Schaltjahr
-        globRad.Eg(mask) = regionProfile.Eg(mask);
+        if day(datetime(year, 12, 31)) == 365  % no leap year
+            mask = globRad.time.Year == year;
+            globRad.Eg(mask) = regionProfile.Eg(mask);
+        end
     end
 end
 
