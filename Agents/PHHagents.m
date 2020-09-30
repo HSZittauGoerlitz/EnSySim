b8bd3b23fb4fdc94af4d3da850fd0bafbd405833
SKittan <S.Kittan@hszg.de>
2020-09-30 08:55:59 +0200

    Restructure constructor for better comprehensibility

--- Agents/PHHagents.m
+++ Agents/PHHagents.m
@@ -23,11 +23,31 @@ classdef PHHagents < AbstractAgent
             %                 demand factors of PHH agents
             %   BSL_PV_dist - Distribution for generating PV auxilary
             %                 demand factors of BSL agents
+            %%%%%%%%%%%%%%%%%%%%%
+            % Common Parameters %
+            %%%%%%%%%%%%%%%%%%%%%
             self.nAgents = nAgents;
             % get random coc from given distribution
             self.getCOC(PHH_COC_dist, 1, 5);
+            % init current balances with 0
+            self.currentEnergyBalance_e = 0;
+            self.currentEnergyBalance_t = 0;
+            %%%%%%%%%%%%%%%%%%%%
+            % Electrical Model %
+            %%%%%%%%%%%%%%%%%%%%
+            % Load
+            %%%%%%
             self.LoadProfile_e = normSLP.PHH .* self.COCfactor .* ...
                                  (0.8 + rand(height(normSLP), self.nAgents));
+            % get static electrical bilance
+            self.staticEnergyBalance_e = self.LoadProfile_e * 0.25;
+            % Generation
+            %%%%%%%%%%%%
+            % init generation array 
+            % only for one time step, since it is dynamic
+            self.Generation_e = zeros(1, nAgents);
+            % PV
+            %%%%
             % generate selection mask for PV generation
             self.maskPV = rand(1, self.nAgents) <= pPVplants;
             self.nPV = sum(self.maskPV);
@@ -44,19 +64,12 @@ classdef PHHagents < AbstractAgent
             self.APV(maskAD) = self.APV(maskAD) .* ...
                                BSL_PV_dist.random(n_maskAD) .* ...
                                (rand(1, n_maskAD) .* 0.4 + 0.8);
-            % init generation array 
-            % only for one time step, since it is dynamic
-            self.Generation_e = zeros(1, nAgents);
+
             % deactivate unused properties
             self.LoadProfile_t = [];
             self.Generation_t = [];
             self.Storage_e = [];
-            self.Storage_t = [];
-            % set thermal Balance to 0
-            self.currentEnergyBalance_t = 0;
-            % get static electrical bilance
-            self.staticEnergyBalance_e = self.LoadProfile_e * 0.25;
-            self.currentEnergyBalance_e = 0;
+            self.Storage_t = [];        
         end
     end
 end
