4bdee650445a549e4523ed6fa4303502b50fdc00
SKittan <S.Kittan@hszg.de>
2020-10-01 14:23:38 +0200

    Fix variable name to fit tearms of literature

--- BoundaryConditions/Thermal/getSpaceHeatingDemand.m
+++ BoundaryConditions/Thermal/getSpaceHeatingDemand.m
@@ -1,12 +1,12 @@
-function E_SHD = getSpaceHeatingDemand(QsHL, ToutN, Tout)
-%getSpaceHeatingDemand> Calculate space heating demand in kW
+function E_SHD = getSpaceHeatingDemand(Q_HLN, ToutN, Tout)
+%getSpaceHeatingDemand Calculate space heating demand in kW
 %   The space heating demand is calculated in relation to outside
 %   temperature and a building specific heating load.
 %   Based on a linear regression model the mean daily heating power is
 %   calculated. The space heating energy demand is determined by
 %   multiplicating this power with 24h.
 % Inputs:
-%   QsHL - specific heat load of building in kW (double)
+%   Q_HLN - Normed heat load of building in kW (double)
 %   ToutN - Normed outside temperature for specific region in °C (double)
 %   Tout - Current (daily mean) outside temperature in °C (double or vector)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -17,7 +17,7 @@ function E_SHD = getSpaceHeatingDemand(QsHL, ToutN, Tout)
     addRequired(p, 'ToutN', @isnumeric);
     addRequired(p, 'Tout', @isnumeric);
 
-    if QsHL <= 0
+    if Q_HLN <= 0
         error('Specific building heat load must be greater than 0.');
     end
     
@@ -25,10 +25,10 @@ function E_SHD = getSpaceHeatingDemand(QsHL, ToutN, Tout)
     if nTout > 1
         E_SHD = zeros(1, nTout);
         mask = Tout < 15;
-        E_SHD(mask) = -QsHL/(15-ToutN) * (Tout(mask)-ToutN) + QsHL;
+        E_SHD(mask) = -Q_HLN/(15-ToutN) * (Tout(mask)-ToutN) + Q_HLN;
     else
         if Tout < 15
-            E_SHD = -QsHL/(15-ToutN) * (Tout-ToutN) + QsHL;
+            E_SHD = -Q_HLN/(15-ToutN) * (Tout-ToutN) + Q_HLN;
         else
             E_SHD = 0;
         end
