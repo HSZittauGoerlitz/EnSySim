24da4b22aa6c54ced55215aa6bfa007284348b07
magMAR12 <SmileAtDeath@web.de>
2020-12-09 21:18:56 +0100

    Add building properties and methods from matlab model

--- pyEnSySim/SystemComponents/building.py
+++ pyEnSySim/SystemComponents/building.py
@@ -3,61 +3,182 @@ from SystemComponents.pv import PV
 
 from numba import types, typed
 from numba.experimental import jitclass
-from numba.extending import overload_method
 
 
 spec = [('agents', types.ListType(Agent.class_type.instance_type)),
+        ('nMaxAgents', types.uint32),
+        ('nAgents', types.uint32)
+        ('Areas', types.float32[:]),  # m^2
+        ('UValues', types.float32[:]),  # W / (K m^2)
+        ('DeltaU', types.float32),  # W / (K m^2)
+        ('nInfiltration', types.float32),  # 1/h
+        ('nVentilation', types.float32),  # 1/h
+        ('isAtDHN', types.boolean)
+        ('V', types.float32),  # m^3
+        ('Q_HLN', types.float32),  # W
         ('PV', PV.class_type.instance_type),
-        ('nMaxAgents', types.int32),
         ]
 
 
 @jitclass(spec)
 class Building():
-    def __init__(self, nMaxAgents, Eg, ToutN):
+    def __init__(self, nMaxAgents, Areas, UValues, DeltaU,
+                 nInfiltration, nVentilation, V, isAtDHN, ToutN):
         """ Create building to simulate buildings energy demand as well
 
         Args:
-            nMaxAgents (int): Number of max. possible agents
-                              living in this building
-            Eg (float): Mean annual global irradiation of parent Cell [kWh/m^2]
-            ToutN (float): Normed outside temperature of parent Cell [°C]
+            nMaxAgents (uint32): Number of max. possible agents
+                                living in this building
+            Areas (float32[:]): All building areas needed for
+                                norm heat load calculation [m^2]
+            UValues (float32[:]): All U-Values needed for
+                                  norm heat load calculation [W/(m^2 K)]
+            DeltaU (float32): Offset for U-Value correction [W/(m^2 K)]
+                              (see _calculateNormHeatLoad for details)
+            nInfiltration (float32): Air infiltration rate of building [^/h]
+            nVentilation (float32): Air infiltration rate due ventilation [^/h]
+            V (float32): Inner building Volume [m^3]
+                         (This Value is used for calculation
+                          of air renewal losses)
+            isAtDHN (bool): If true building is connected to the
+                            district heating network
+            ToutN (float32): Normed outside temperature for
+                             region of building [°C]
         """
-        self._createAgents(nMaxAgents)
-        self._createPV(Eg)
-
-    def _createAgents(self, nMaxAgents):
+        # check parameter
         if nMaxAgents <= 0:
             raise ValueError("Number of max. Agents must be greater than 0")
 
+        if len(Areas) != len(UValues):
+            raise ValueError("Number of Area values must match "
+                             "number of U-Values")
+        else:
+            for idx in range(len(Areas)):
+                if Areas[idx] < 0:
+                    raise ValueError("Area {} is smaller than 0".
+                                     format(idx))
+                if UValues[idx] < 0:
+                    raise ValueError("U-Value {} is smaller than 0".
+                                     format(idx))
+
+        if DeltaU <= 0:
+            raise ValueError("U-Value offset must not be negative")
+
+        if (nInfiltration < 0) | (nVentilation < 0):
+            raise ValueError("Infiltration rate must not be negative")
+
+        if V < 0:
+            raise ValueError("Building volume must not be negative")
+
+        # set attributes
         self.nMaxAgents = nMaxAgents
-        agents = []
+        self.nAgents = 0
+        self.agents = typed.List.empty_list(Agent.class_type.instance_type)
+        self.Areas = Areas
+        self.UValues = UValues
+        self.DeltaU = DeltaU
+        self.nInfiltration = nInfiltration
+        self.nVentilation = nVentilation
+        self.V = V
+        self._addNormHeatingLoad(ToutN)
+        self.isAtDHN = isAtDHN
+        # all other possible components are empty
+        self.PV = None
 
-        for idx in range(nMaxAgents):
-            agents.append(Agent('PHH'))
+    def addAgent(self, agent):
+        if self.nAgents + 1 <= self.nMaxAgents:
+            self.agents.append(agent)
+            self.nAgents += 1
+        else:
+            print("WARNING: Number of max. Agents ({}) reached, "
+                  "no agent is added".format(self.nMaxAgents))
 
-        self.agents = typed.List(agents)
+    def addPV(self, PV):
+        if not self.PV:
+            self.PV = PV
+        else:
+            print("WARNING: Building already has a PV plant, nothing is added")
 
-    def _createPV(self, Eg):
-        """ Create PV object for building """
-        sumCOC = 0
-        sumAPVdemand = 0
-        nAgents = 0
-        for agent in self.agents:
-            sumCOC += agent.COC
-            sumAPVdemand += agent.demandAPV
-            nAgents += 1
+    def addDimensionedPV(self, Eg):
+        """ Add PV with dimensioning of installed PV power
+        by agents demand statistics
+        and environments global irradiation history.
 
-        self.PV = PV(Eg, sumCOC, sumAPVdemand/nAgents)
+        Args:
+            Eg (float32): Mean annual global irradiation
+                          for simulated region [kWh/m^2]
+        """
+        if not self.PV:
+            sumCOC = 0
+            sumAPVdemand = 0
+            nAgents = 0
+            for agent in self.agents:
+                sumCOC += agent.COC
+                sumAPVdemand += agent.demandAPV
+                nAgents += 1
 
-    def _step(self, SLPdata, HWprofile):
+            self.PV = PV(Eg, sumCOC, sumAPVdemand/nAgents)
+        else:
+            print("WARNING: Building already has a PV plant, nothing is added")
+
+    def _addNormHeatingLoad(self, ToutN):
+        """ Calculate normed heating load Q_HLN of a building [W]
+
+        The calculation is done in reference to the simplified method
+        of DIN EN 12831-1:2017-09
+        Modifications / Simplifications:
+            - Consideration of the whole building:
+                o normed room temperature is set to 20°C
+                o temperature matching coefficient is set to 1
+            - Normed air heat losses include infiltration losses
+
+        Args:
+            ToutN (float32): Normed outside temperature for
+                             region of building [°C]
+        """
+        # Temperature Difference
+        dT = (20. - ToutN)
+        # Transmission losses
+        PhiT = 0.
+        for i in range(len(self.Areas)):
+            PhiT += self.Areas[i] * (self.UValues[i] + self.DeltaU)
+        PhiT *= dT
+        # Air renewal losses
+        PhiA = self.V * (self.nInfiltration + self.nVentilation) * 0.3378 * dT
+
+        self.Q_HLN = PhiT + PhiA
+
+    def _getSpaceHeatingDemand(self, Tout, ToutN):
+        """Calculate space heating demand in W
+
+        The space heating demand is calculated in relation to outside
+        temperature and a building specific heating load.
+        Based on a linear regression model the mean daily heating power is
+        calculated. The space heating energy demand is determined by
+        multiplicating this power with 24h.
+
+        Args:
+            Tout (float32): Current (daily mean) outside temperature [°C]
+            ToutN (float32): Normed outside temperature for
+                             region of building [°C]
+
+        Returns:
+            float32: Space heating demand [W]
+        """
+        if Tout < 15:
+            return self.Q_HLN * (ToutN-Tout) / (15-ToutN) + self.Q_HLN
+        else:
+            return 0.
+
+    def _step(self, SLPdata, HWprofile, Tout, ToutN):
         """ Calculate and return current energy balance
 
         Args:
             SLPdata (dict with float): Standard load Profile of all agent types
             HWprofile (float): Actual hot water profile value [W]
-
-
+            Tout (float32): Current (daily mean) outside temperature [°C]
+            ToutN (float32): Normed outside temperature for
+                             region of building [°C]
         Returns:
             [(float, float)]: Current electrical and thermal energy balance [W]
         """
@@ -69,18 +190,22 @@ class Building():
         electrical_balance = 0.
         thermal_balance = 0.
 
+        # calculate loads
         for agent in self.agents:
             ae, at = agent._step(SLPdata, HWprofile)
             electrical_load += ae
             thermal_load += at
 
+        thermal_load += self._getSpaceHeatingDemand(Tout, ToutN)
+
+        # calculate generation
+        # TODO: PV, CHP
+        if not self.isAtDHN:
+            # Building is self-supplied
+            thermal_generation = thermal_load
+
+        # Calculate resulting energy balance
         electrical_balance = electrical_generation - electrical_load
         thermal_balance = thermal_generation - thermal_load
 
         return (electrical_balance, thermal_balance)
-
-    def clone(inst,):
-        if inst is Building.class_type.instance_type:
-            def impl(inst,):
-                return Building(inst.value)
-            return impl
