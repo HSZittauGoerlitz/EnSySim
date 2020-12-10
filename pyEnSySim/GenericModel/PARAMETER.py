0cef4c5ba25b957b91ade45bbcd688db987e7a2b
magMAR12 <SmileAtDeath@web.de>
2020-12-10 14:38:40 +0100

    Add centralised model parameters
    These parameters are relevant for the actual study.

---
+++ pyEnSySim/GenericModel/PARAMETER.py
@@ -0,0 +1,23 @@
+# Building data for now
+# TODO: Add source
+FSH = {}
+REH = {}
+SAH = {}
+BAH = {}
+PBTYPES_NOW = {}
+FSH['Class'] = [0.2587, 0.383, 0.1767, 0.1816, 0.0]
+FSH['Modern'] = [0.4704, 0.4712, 0.2897, 0.0485, 0.0]
+FSH['AirMech'] = [0.02, 0.05, 0.05, 0.1, 0.0]
+REH['Class'] = [0.2409, 0.3788, 0.2032, 0.1771, 0.0]
+REH['Modern'] = [0.4758, 0.4712, 0.2928, 0.0492, 0.0]
+REH['AirMech'] = [0.02, 0.05, 0.05, 0.1, 0.0]
+SAH['Class'] = [0.2589, 0.4371, 0.1443, 0.1597, 0.0]
+SAH['Modern'] = [0.4797, 0.4815, 0.3798, 0.1363, 0.0]
+SAH['AirMech'] = [0.02, 0.05, 0.05, 0.1, 0.0]
+BAH['Class'] = [0.0681, 0.6185, 0.3134]
+BAH['Modern'] = [0.4842, 0.4799, 0.3683]
+BAH['AirMech'] = [0.02, 0.05, 0.05]
+PBTYPES_NOW['FSH'] = FSH
+PBTYPES_NOW['REH'] = REH
+PBTYPES_NOW['SAH'] = SAH
+PBTYPES_NOW['BAH'] = BAH
