Fix mask calculation of BC
There was an length missmatch of Simulation and BC data,
since the BC doy is for a year without leap day and leap day data
is on day 0. Hence for BC the doy-1 must be compared with minDay

pyEnSySim/BoundaryConditions/Simulation/SimulationData.py
@@ -270,7 +270,7 @@ def _getWeather(simData, region):
 
             # third is time after leap day
             maxDay = simData.doy[maskY].max()
-            maskBC = ((weatherBC.doy >= minDay) &
+            maskBC = ((weatherBC.doy >= minDay-1) &  # 60 is normal day
                       (weatherBC.doy <= maxDay))
             subMaskY = (maskY &
                         (simData.doy[maskY] >= minDay) &
@@ -349,4 +349,3 @@ def getSimData(startDate, endDate, region):
             data.loc[:, 'T'].to_numpy(dtype=np.float32),
             data.Eg.to_numpy(dtype=np.float32)
             )
-