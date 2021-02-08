f0ac317f9cf5db28711e929a1f1cf67a28c0ac63
mherling <martin.herling@hszg.de>
2021-02-08 13:35:39 +0100

    Add taulated heatump coeffient data
    Thomas Kemmler und Bernd Thomas. „Simulation von Wärmepumpensystemem auf der Grundlage von Korrelationsfunktionen für die Leistungsdaten der Wärmepumpe“, 2020.

--- pyEnSySim/BoundaryConditions/Thermal/createHeatpumpCoefficients.py
+++ pyEnSySim/BoundaryConditions/Thermal/createHeatpumpCoefficients.py
@@ -1,14 +1,18 @@
+# %% imports 
 import pandas as pd
 
+# %% class names
 class_1 = '5to18kW'
 class_2 = '18to35kW'
 class_3 = '35to80kW'
 
+# %% read from xls
 df_sheet_multi = pd.read_excel('heatpumpCoefficients.xls',
                                sheet_name=[class_1,
                                            class_2,
                                            class_3])
 
+# %% store to hdf
 name = 'HeatpumpCoefficients'
 
 store = pd.HDFStore(name + '.h5')
