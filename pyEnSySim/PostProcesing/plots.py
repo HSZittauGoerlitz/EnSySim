d26701281c506d22b2d9ec3b238e6c9fb1b2717b
magMAR12 <SmileAtDeath@web.de>
2021-02-25 07:07:21 +0100

    Add plot for arbitrary power data

--- pyEnSySim/PostProcesing/plots.py
+++ pyEnSySim/PostProcesing/plots.py
@@ -8,6 +8,85 @@ COL_CON = 'rgb(255,84,79)'  # color for consumption
 COL_BAL = 'rgba(0,0,0,0.5)'  # color for balance
 
 
+def arbitraryBalance(generation, load, time, unitPrefix, title=""):
+    """Plot the power and energy balance for given curves
+
+    Arguments:
+        generation {np array} -- Course of generated power of type 'type_' [W]
+        load {np array} -- Course of power load of type 'type_' [W]
+        unitPrefix {str} -- Prefix used for power data, the data must be
+                            converted accordingly
+        time (pd series of datetime): Time
+
+    Keyword Arguments:
+        title {str} -- Title used for plot (default: {""})
+    """
+    # calculate resulting energy course
+    dt = time.diff().dt.seconds / 3600.  # time difference in h
+    energy_gen = generation.cumsum()
+    energy_gen[1:] *= dt[1:]
+    energy_load = load.cumsum()
+    energy_load[1:] *= dt[1:]
+
+    # Create Figure
+    fig = make_subplots(rows=2, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.02)
+    # Add Lines
+    fig.add_trace(go.Scatter(x=time, y=generation,
+                             line={'color': COL_GEN,
+                                   'width': 1},
+                             name="Generation E",
+                             legendgroup='Electrical'
+                             ),
+                  row=1, col=1)
+    fig.add_trace(go.Scatter(x=time, y=-load,
+                             line={'color': COL_CON,
+                                   'width': 1},
+                             name="Load E",
+                             legendgroup='Electrical'
+                             ),
+                  row=1, col=1)
+    fig.add_trace(go.Scatter(x=time, y=generation-load,
+                             line={'color': COL_BAL,
+                                   'width': 1},
+                             name="Balance E",
+                             legendgroup='Electrical'
+                             ),
+                  row=1, col=1)
+    fig.add_trace(go.Scatter(x=time, y=energy_gen,
+                             line={'color': COL_GEN,
+                                   'width': 1},
+                             name="Generation T",
+                             legendgroup='Thermal'
+                             ),
+                  row=2, col=1)
+    fig.add_trace(go.Scatter(x=time, y=-energy_load,
+                             line={'color': COL_CON,
+                                   'width': 1},
+                             name="Load T",
+                             legendgroup='Thermal'
+                             ),
+                  row=2, col=1)
+    fig.add_trace(go.Scatter(x=time, y=energy_gen-energy_load,
+                             line={'color': COL_BAL,
+                                   'width': 1},
+                             name="Balance T",
+                             legendgroup='Thermal'
+                             ),
+                  row=2, col=1)
+    fig.update_layout(height=600, width=1000,
+                      title_text=title)
+    # add axis labels
+    fig.update_xaxes(title_text="Time", row=2, col=1)
+    fig.update_yaxes(title_text="Power [{}W]".format(unitPrefix),
+                     row=1, col=1)
+    fig.update_yaxes(title_text="Energy [{}W]".format(unitPrefix),
+                     row=2, col=1)
+    # show figure
+    fig.show()
+
+
 def cellPowerBalance(cell, time):
     """ Plot the electrical and thermal power balance of a cell
         for a simulation with known simulation time.
@@ -90,6 +169,7 @@ def cellEnergyBalance(cell, time, step=0.25):
         time (pd series of datetime): Time
         step (float): Time step of time data [h] (Default: 0.25h)
     """
+    # TODO: Use steps of given time data
     # get data
     gen_e = np.array(cell.gen_e.get_memory()) * 1e-6
     gen_t = np.array(cell.gen_t.get_memory()) * 1e-6
@@ -165,7 +245,7 @@ def cellEnergyBalance(cell, time, step=0.25):
     fig.show()
 
 
-def buildingTemperature(building, time, T, step=0.25):
+def buildingTemperature(building, time, T):
     """ Plot the temperature course for given building
 
     Args:
@@ -173,7 +253,6 @@ def buildingTemperature(building, time, T, step=0.25):
                                should be plotted
           time (pd series of datetime): Time
           T (np array): Outside Temperature during Simulation [degC]
-          step (float): Time step of time data [h] (Default: 0.25h)
     """
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=time, y=building.temperature_hist.get_memory(),
