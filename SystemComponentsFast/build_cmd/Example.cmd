c84076270ea225d27558065deb0be883b0a1bd06
magMAR12 <SmileAtDeath@web.de>
2021-03-19 10:33:16 +0100

    Rename to make clear what example is meant

--- SystemComponentsFast/build_cmd/Example.cmd
+++
@@ -1,4 +0,0 @@
-call PATH_TO_CONDA_ACTIVATE PYTHON_ENV_NAME
-cargo +nightly build --release
-ren .\target\release\SystemComponentsFast.dll SystemComponentsFast.pyd
-move .\target\release\SystemComponentsFast.pyd ..\SystemComponentsFast.pyd
