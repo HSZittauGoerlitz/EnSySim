79c8626fe2128fa39b34e31468cfeb0102a490a6
SKittan <SmileAtDeath@web.de>
2021-04-12 10:26:47 +0200

    Add SynPro weather data and thermal plots

--- Tests/CompareDemand.py
+++ Tests/CompareDemand.py
@@ -16,6 +16,7 @@ from BoundaryConditions.Simulation.SimulationData import getSimData
 from GenericModel.Design import _addAgents, _loadBuildingData
 import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
 from SystemComponentsFast import simulate, Building, Cell
 from PostProcesing import plots
 import logging
@@ -81,13 +82,26 @@ building.replace_agent(0, agent)
 # Add building to cell
 cell.add_building(building)
 
+# load SynPro weather data and replace temperature
+weather = pd.read_hdf("Tests/Data/TRY_2010_Potsdam.h5", key='Weather')
+fT = interp1d((weather.time - weather.time[0]).dt.total_seconds(),
+               weather['T [degC]'])
+T = np.array(fT((time - time[0]).dt.total_seconds()), dtype=np.float32)
+
 # %% Run simulation
 simulate(cell, nSteps, SLP_PHH, SLP_BSLa, SLP_BSLc, HWP, T, Eg)
+
+# %% recalculate agents hot water demand
+# this recalculation does not correspond exactly the simulation course
+# due to the random portion in the model
+# -> this portion is neglected here, since it's balanced in long time course
+agent_hw = agent.hw_demand * HWP * 1e-3  # in kW
+building_sh = np.array(cell.load_t.get_memory())*1e-3 - agent_hw  # in kW
 
 # %% load data to compare
 SynProData = pd.read_hdf("Tests/Data/SynProTestHouse_ThreeDefaultPersons.h5")
 
-# %%
+# %% compare electrical demand
 plots.compareCurves([SynProData.time],
                     [SynProData['P_Electrical [W]']*1e-3,
                      np.array(cell.load_e.get_memory())*1e-3],
@@ -98,5 +112,29 @@ plots.compareCurves([SynProData.time],
                     [SynProData['P_Electrical [W]'].cumsum()*1e-3*0.25,
                      np.array(cell.load_e.get_memory()).cumsum()*1e-3*0.25],
                     ['SynPro', 'EnSySim'], yLabel='Electrical Energy in kWh')
+
+# %% compare space heating demand
+plots.compareCurves([SynProData.time],
+                    [SynProData['Q_SpaceHeating [W]']*1e-3,
+                     building_sh],
+                    ['SynPro', 'EnSySim'], yLabel='Thermal Power in kW',
+                    title='Comparison of buildings heat losses')
+
+plots.compareCurves([SynProData.time],
+                    [SynProData['Q_SpaceHeating [W]'].cumsum()*1e-6*0.25,
+                     building_sh.cumsum()*1e-3*0.25],
+                    ['SynPro', 'EnSySim'], yLabel='Thermal Energy in MWh')
+
+# %% compare hot water demand
+plots.compareCurves([SynProData.time],
+                    [SynProData['Q_HotWater [W]']*1e-3,
+                     agent_hw],
+                    ['SynPro', 'EnSySim'], yLabel='Thermal Power in kW',
+                    title='Comparison of residents hot water demand')
+
+plots.compareCurves([SynProData.time],
+                    [SynProData['Q_HotWater [W]'].cumsum()*1e-6*0.25,
+                     agent_hw.cumsum()*1e-3*0.25],
+                    ['SynPro', 'EnSySim'], yLabel='Thermal Energy in MWh')
 
 # %%
