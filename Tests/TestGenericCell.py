9aaa5a0e95292e6e5d83f09e6981bd615901f87a
SKittan <SmileAtDeath@web.de>
2021-04-16 12:52:51 +0200

    Convert generic test cell case to py-script
    This allows easier debugging and avoids issues due
    saving ipynb with outbut data.

---
+++ Tests/TestGenericCell.py
@@ -0,0 +1,63 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+# Imports
+from BoundaryConditions.Simulation.SimulationData import getSimData
+from GenericModel.Design import generateGenericCell
+from GenericModel.PARAMETER import PBTYPES_NOW as pBTypes
+from SystemComponentsFast import simulate
+from PostProcesing import plots
+import logging
+
+
+# %%
+FORMAT = ("%(levelname)s %(name)s %(asctime)-15s "
+          "%(filename)s:%(lineno)d %(message)s")
+logging.basicConfig(format=FORMAT)
+logging.getLogger().setLevel(logging.WARNING)
+
+# %%
+# Parameter
+# time
+start = '01.01.2020'
+end = '01.01.2021'
+# agents
+nSepBSLagents = 100
+pAgricultureBSLsep = 0.7
+nBuildings = {'FSH': 505, 'REH': 1010, 'SAH': 680, 'BAH': 100}
+pAgents = {'FSH': 0.9, 'REH': 0.9, 'SAH': 0.85, 'BAH': 0.75}
+pPHHagents = {'FSH': 0.8, 'REH': 0.8, 'SAH': 0.6, 'BAH': 0.9}
+pAgriculture = {'FSH': 0.2, 'REH': 0.2, 'SAH': 0.0, 'BAH': 0.0}
+# district heating and PV
+pDHN = {'FSH': 0.1, 'REH': 0.1, 'SAH': 0.1, 'BAH': 0.1}
+pPVplants = 0.2
+pHeatpumps = {'class_1': 0, 'class_2': 0,
+              'class_3': 0, 'class_4': 0.12,
+              'class_5': 0.27}
+pCHP = 0.1
+# buildings are imported
+# environment
+region = "East"
+
+# %%
+# prepare simulation
+nSteps, time, SLP_PHH, SLP_BSLa, SLP_BSLc, HWP, T, Eg = getSimData(start,
+                                                                   end, region)
+
+# %%
+cell = generateGenericCell(nBuildings, pAgents,
+                           pPHHagents, pAgriculture,
+                           pDHN, pPVplants, pHeatpumps, pCHP, pBTypes,
+                           nSepBSLagents, pAgricultureBSLsep,
+                           region, nSteps)
+
+# %%
+simulate(cell, nSteps, SLP_PHH, SLP_BSLa, SLP_BSLc, HWP, T, Eg)
+
+# %%
+plots.cellPowerBalance(cell, time)
+
+# %%
+plots.cellEnergyBalance(cell, time)
+
+# %%
