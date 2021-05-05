b85679823ef3cc2b00eaec7e6f0276de7867ee88
SKittan <SmileAtDeath@web.de>
2021-05-05 14:49:29 +0200

    Update Test for CHP system

--- Tests/TestCHP.py
+++ Tests/TestCHP.py
@@ -1,11 +1,10 @@
 # %% Imports
 from BoundaryConditions.Simulation.SimulationData import getSimData
 from GenericModel.Design import _addAgents, _loadBuildingData
+from itertools import product
 import numpy as np
 import pandas as pd
-import plotly.graph_objs as go
 from SystemComponentsFast import simulate, Building, Cell
-from PostProcesing import dataCollection, plots
 import logging
 
 # %% logger config
@@ -34,101 +33,94 @@ cell = Cell(climate.loc['EgNorm [kWh/m^2]', 'Value'],
             climate.loc['ToutNorm [degC]', 'Value'],
             nSteps)
 
-# %% Create Building
 # Parameter
-bClass = "class_3"
-mState = "original"  # "modernised"
-airState = "VentilationFree"  # "VentilationMech"
+classes = ["class_" + str(nr) for nr in range(1, 6)]
+mStates = ["original", "modernised"]
+airStates = ["VentilationFree", "VentilationMech"]
+
 isAtDHN = False
-a_uv_values = np.array([Geo.loc['Areas'].values.T[0],
-                        U.loc['UValues', (bClass, mState)]
-                        ]).T
-if bClass == 'class_5':
-    infState = 'new'
-else:
-    infState = mState
 
-# Building and Occupants
-building = Building(Geo.loc['nUnits'].values.astype(np.uint32)[0][0],
-                    Geo.loc[('A_living', ''), 'Value'], a_uv_values,
-                    U.loc['DeltaU', (bClass, mState)],
-                    n.loc['Infiltration', infState],
-                    n.loc[airState, infState],
-                    (Geo.loc['cp_effective'] * Geo.loc['Volume']).Value,
-                    g.loc[mState, bClass],
-                    Geo.loc[('Volume')].values.astype(np.uint32)[0][0],
-                    isAtDHN, cell.t_out_n, nSteps
-                    )
-# Create and add agents
-_addAgents(building, 1., 1., 0.)
+resParameter = ["Electrical Energy Generated [MWh]",
+                "Electrical Energy Consumed [MWh]",
+                "Thermal Energy Generated [MWh]",
+                "Thermal Energy Consumed [MWh]",
+                "Max. Building Temperature [degC]",
+                "Min. Building Temperature [degC]",
+                "Mean. Building Temperature [degC]",
+                "CHP Full Load Hours [h]"]
 
-# Add components
-building.add_dimensioned_chp(nSteps)
+# data frame for results
+results = pd.DataFrame(columns=pd.MultiIndex.from_tuples(product(classes,
+                                                                 mStates,
+                                                                 airStates)),
+                       index=resParameter)
 
-# Add building to cell
-cell.add_building(building)
 
-# %% Run simulation
-simulate(cell, nSteps, SLP.to_dict('list'), HWP, Weather.to_dict('list'),
-         Solar.to_dict('list'))
+# %% Create Building and Simulate
+for bClass, mState, airState in results.columns.to_list():
+    a_uv_values = np.array([Geo.loc['Areas'].values.T[0],
+                            U.loc['UValues', (bClass, mState)]
+                            ]).T
+    if bClass == 'class_5':
+        infState = 'new'
+    else:
+        infState = mState
 
-# %%
-plots.cellPowerBalance(cell, time)
+    # cell
+    cell = Cell(climate.loc['EgNorm [kWh/m^2]', 'Value'],
+                climate.loc['ToutNorm [degC]', 'Value'],
+                nSteps)
 
-# %%
-plots.cellEnergyBalance(cell, time)
+    # Building and Occupants
+    building = Building(Geo.loc['nUnits'].values.astype(np.uint32)[0][0],
+                        Geo.loc[('A_living', ''), 'Value'], a_uv_values,
+                        U.loc['DeltaU', (bClass, mState)],
+                        n.loc['Infiltration', infState],
+                        n.loc[airState, infState],
+                        (Geo.loc['cp_effective'] * Geo.loc['Volume']).Value,
+                        g.loc[mState, bClass],
+                        Geo.loc[('Volume')].values.astype(np.uint32)[0][0],
+                        isAtDHN, cell.t_out_n, nSteps
+                        )
+    # Create and add agents
+    _addAgents(building, 1., 1., 0.)
 
-# %%
-gen_t, load_t = dataCollection.getBuildingsThermalBalance(cell)
-plots.arbitraryBalance(gen_t*1e-3, load_t*1e-3, time, 'k',
-                       'Thermal balance in test building')
+    # Add components
+    building.add_dimensioned_chp(nSteps)
 
-# %%
-b = cell.buildings[0]
+    # Add building to cell
+    cell.add_building(building)
 
-chp_gen_e = np.array(b.chp_system.gen_e.get_memory())
-CHPstate = chp_gen_e > 0.
+    simulate(cell, nSteps, SLP.to_dict('list'), HWP, Weather.to_dict('list'),
+             Solar.to_dict('list'))
 
-fig_T = plots.buildingTemperature(b, time, Weather['T [degC]'], retFig=True)
-fig_S = plots.chargeState(b.chp_system.storage, time, retFig=True)
-fig_Shw = plots.chargeState(b.chp_system.storage_hw, time, retFig=True)
+    # get results
+    b = cell.buildings[0]
+    gen_e = np.array(b.gen_e.get_memory())
+    load_e = np.array(b.load_e.get_memory())
+    gen_t = np.array(b.gen_t.get_memory())
+    load_t = np.array(b.load_t.get_memory())
+    bT = np.array(b.temperature_hist.get_memory())
+    chp_gen_e = np.array(b.chp_system.gen_e.get_memory())
 
-fig_S.update_traces({'name': 'Storage'}, selector={'name': "charge"})
-fig_Shw.update_traces({'name': 'Storage hw'}, selector={'name': "charge"})
-
-fig_T = fig_T.set_subplots(rows=3, cols=1,
-                           shared_xaxes=True,
-                           vertical_spacing=0.02,
-                           subplot_titles=("",
-                                           fig_S.layout.title.text,
-                                           "Hot Water " +
-                                           fig_Shw.layout.title.text
-                                           ),
-                           specs=[[{"secondary_y": True}],
-                                  [{"secondary_y": False}],
-                                  [{"secondary_y": False}]
-                                  ]
-                           )
-
-fig_T.add_trace(go.Scatter(x=time, y=CHPstate,
-                           line={'color': 'rgba(100, 149, 237, 0.5)',
-                                 'width': 1},
-                           name="CHP state"),
-                secondary_y=True
-                )
-
-fig_T.update_layout({'height': 1000, 'width': 1000,
-                     'title': "Building overview"})
-fig_T.update_xaxes(title_text="", row=1, col=1)
-fig_T.update_xaxes(title_text="", row=2, col=1)
-fig_T.update_xaxes(title_text="Time", row=3, col=1)
-fig_T.append_trace(fig_S['data'][0], row=2, col=1)
-fig_T.append_trace(fig_Shw['data'][0], row=3, col=1)
-fig_T.update_yaxes(fig_S.layout['yaxis'], row=2, col=1)
-fig_T.update_yaxes(fig_Shw.layout['yaxis'], row=3, col=1)
+    results.loc['Electrical Energy Generated [MWh]',
+                (bClass, mState, airState)] = gen_e.sum() * 0.25 * 1e-6
+    results.loc['Electrical Energy Consumed [MWh]',
+                (bClass, mState, airState)] = load_e.sum() * 0.25 * 1e-6
+    results.loc['Thermal Energy Generated [MWh]',
+                (bClass, mState, airState)] = gen_t.sum() * 0.25 * 1e-6
+    results.loc['Thermal Energy Consumed [MWh]',
+                (bClass, mState, airState)] = load_t.sum() * 0.25 * 1e-6
+    results.loc['Max. Building Temperature [degC]',
+                (bClass, mState, airState)] = bT.max()
+    results.loc['Min. Building Temperature [degC]',  # exclude first week
+                (bClass, mState, airState)] = bT[7*96:].min()
+    results.loc['Mean. Building Temperature [degC]',
+                (bClass, mState, airState)] = bT.mean()
+    results.loc['CHP Full Load Hours [h]',
+                (bClass, mState, airState)] = (chp_gen_e > 0.).sum() * 0.25
 
 # %%
-print("The CHP has {:.2f} full load hours"
-      .format((chp_gen_e > 0.).sum() * 0.25))
+results
 
 # %%
---
+++ Tests/TestCHPsingleBuilding.py
@@ -0,0 +1,134 @@
+# %% Imports
+from BoundaryConditions.Simulation.SimulationData import getSimData
+from GenericModel.Design import _addAgents, _loadBuildingData
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+from SystemComponentsFast import simulate, Building, Cell
+from PostProcesing import dataCollection, plots
+import logging
+
+# %% logger config
+FORMAT = ('%(levelname)s %(name)s %(asctime)-15s '
+          '%(filename)s:%(lineno)d %(message)s')
+logging.basicConfig(format=FORMAT)
+logging.getLogger().setLevel(logging.WARNING)
+
+# %% Parameter
+# time
+start = "01.01.2020"
+end = "01.01.2021"
+# environment
+region = "East"
+
+bType = "FSH"
+
+# %% prepare simulation
+nSteps, time, SLP, HWP, Weather, Solar = getSimData(start, end, region)
+climate = pd.read_hdf("./BoundaryConditions/Weather/" + region +
+                      ".h5", 'Standard')
+
+Geo, U, g, n = _loadBuildingData(bType)
+
+cell = Cell(climate.loc['EgNorm [kWh/m^2]', 'Value'],
+            climate.loc['ToutNorm [degC]', 'Value'],
+            nSteps)
+
+# %% Create Building
+# Parameter
+bClass = "class_3"
+mState = "original"  # "modernised"
+airState = "VentilationFree"  # "VentilationMech"
+isAtDHN = False
+a_uv_values = np.array([Geo.loc['Areas'].values.T[0],
+                        U.loc['UValues', (bClass, mState)]
+                        ]).T
+if bClass == 'class_5':
+    infState = 'new'
+else:
+    infState = mState
+
+# Building and Occupants
+building = Building(Geo.loc['nUnits'].values.astype(np.uint32)[0][0],
+                    Geo.loc[('A_living', ''), 'Value'], a_uv_values,
+                    U.loc['DeltaU', (bClass, mState)],
+                    n.loc['Infiltration', infState],
+                    n.loc[airState, infState],
+                    (Geo.loc['cp_effective'] * Geo.loc['Volume']).Value,
+                    g.loc[mState, bClass],
+                    Geo.loc[('Volume')].values.astype(np.uint32)[0][0],
+                    isAtDHN, cell.t_out_n, nSteps
+                    )
+# Create and add agents
+_addAgents(building, 1., 1., 0.)
+
+# Add components
+building.add_dimensioned_chp(nSteps)
+
+# Add building to cell
+cell.add_building(building)
+
+# %% Run simulation
+simulate(cell, nSteps, SLP.to_dict('list'), HWP, Weather.to_dict('list'),
+         Solar.to_dict('list'))
+
+# %%
+plots.cellPowerBalance(cell, time)
+
+# %%
+plots.cellEnergyBalance(cell, time)
+
+# %%
+gen_t, load_t = dataCollection.getBuildingsThermalBalance(cell)
+plots.arbitraryBalance(gen_t*1e-3, load_t*1e-3, time, 'k',
+                       'Thermal balance in test building')
+
+# %%
+b = cell.buildings[0]
+
+chp_gen_e = np.array(b.chp_system.gen_e.get_memory())
+CHPstate = chp_gen_e > 0.
+
+fig_T = plots.buildingTemperature(b, time, Weather['T [degC]'], retFig=True)
+fig_S = plots.chargeState(b.chp_system.storage, time, retFig=True)
+fig_Shw = plots.chargeState(b.chp_system.storage_hw, time, retFig=True)
+
+fig_S.update_traces({'name': 'Storage'}, selector={'name': "charge"})
+fig_Shw.update_traces({'name': 'Storage hw'}, selector={'name': "charge"})
+
+fig_T = fig_T.set_subplots(rows=3, cols=1,
+                           shared_xaxes=True,
+                           vertical_spacing=0.02,
+                           subplot_titles=("",
+                                           fig_S.layout.title.text,
+                                           "Hot Water " +
+                                           fig_Shw.layout.title.text
+                                           ),
+                           specs=[[{"secondary_y": True}],
+                                  [{"secondary_y": False}],
+                                  [{"secondary_y": False}]
+                                  ]
+                           )
+
+fig_T.add_trace(go.Scatter(x=time, y=CHPstate,
+                           line={'color': 'rgba(100, 149, 237, 0.5)',
+                                 'width': 1},
+                           name="CHP state"),
+                secondary_y=True
+                )
+
+fig_T.update_layout({'height': 1000, 'width': 1000,
+                     'title': "Building overview"})
+fig_T.update_xaxes(title_text="", row=1, col=1)
+fig_T.update_xaxes(title_text="", row=2, col=1)
+fig_T.update_xaxes(title_text="Time", row=3, col=1)
+fig_T.append_trace(fig_S['data'][0], row=2, col=1)
+fig_T.append_trace(fig_Shw['data'][0], row=3, col=1)
+fig_T.update_yaxes(fig_S.layout['yaxis'], row=2, col=1)
+fig_T.update_yaxes(fig_Shw.layout['yaxis'], row=3, col=1)
+
+# %%
+print("The CHP has {:.2f} full load hours"
+      .format((chp_gen_e > 0.).sum() * 0.25))
+
+# %%
