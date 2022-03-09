b1f862f3e135e72403b3b1088d921288dda40ccf
mherling <mherling@posteo.de>
2022-03-09 08:38:45 +0100

    Add 2030 scenario

---
+++ Tests/GenericCellToday2030.py
@@ -0,0 +1,103 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+# Imports
+from BoundaryConditions.Simulation.SimulationData import getSimData
+from Controller.Cell.CHP_SystemThermal import CtrlDefault
+from GenericModel.Design import _check_pBTypes, generateGenericCell
+from GenericModel.PARAMETER import PBTYPES_2030 as pBTypes
+from SystemComponentsFast import simulate, CellChpSystemThermal
+from PostProcesing import plots
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+import numpy as np
+import logging
+
+
+# %%
+FORMAT = ("%(levelname)s %(name)s %(asctime)-15s "
+          "%(filename)s:%(lineno)d %(message)s")
+logging.basicConfig(format=FORMAT)
+logging.getLogger().setLevel(logging.DEBUG)
+
+# %%
+# set parameters
+# time
+start = '01.01.2020'
+end = '01.01.2021'
+# seperate agents
+nSepBSLagents = 100
+pAgricultureBSLsep = 0.7
+# pHH buildings
+nBuildings = {'FSH': 505, 'REH': 1010, 'SAH': 680, 'BAH': 100}
+pAgents = {'FSH': 0.9, 'REH': 0.9, 'SAH': 0.85, 'BAH': 0.75}
+pPHHagents = {'FSH': 0.8, 'REH': 0.8, 'SAH': 0.6, 'BAH': 0.9}
+pAgriculture = {'FSH': 0.2, 'REH': 0.2, 'SAH': 0.0, 'BAH': 0.0}
+# district heating and PV
+pDHN = {'FSH': 1, 'REH': 1, 'SAH': 1, 'BAH': 1}
+pPVplants = 0.2
+pHeatpumps = {'class_1': 0, 'class_2': 0,
+              'class_3': 1, 'class_4': 1,
+              'class_5': 1}
+pCHP = 0.03
+# buildings are imported
+# environment
+region = "East"
+
+# set controller to use it or set variable to None
+controller = CtrlDefault()
+
+# %%
+# prepare simulation
+nSteps, time, SLP, HWP, Weather, Solar = getSimData(start, end, region)
+
+# %%
+# generate cell
+cell = generateGenericCell(nBuildings, pAgents,
+                           pPHHagents, pAgriculture,
+                           pDHN, pPVplants, pHeatpumps, pCHP, pBTypes,
+                           nSepBSLagents, pAgricultureBSLsep,
+                           region, nSteps)
+
+""" # get dhn demand
+demand = cell.get_thermal_demand(True)
+# generate chp system with storage
+chpSystem = CellChpSystemThermal(demand, 0.35, 2*demand, 0.05,
+                                 0.98, 0.98, nSteps)
+# configure controller
+chpSystem.controller = None#controller
+# add chp system to cell
+cell.add_chp_thermal(chpSystem) """
+
+# %%
+# run the simulation
+simulate(cell, nSteps, SLP.to_dict('list'), HWP, Weather.to_dict('list'),
+         Solar.to_dict('list'))
+
+# %%
+plots.cellPowerBalance(cell, time)
+
+# %%
+plots.cellEnergyBalance(cell, time)
+
+# %%
+chpSystem = cell.get_thermal_chp_system()
+chp_gen_e = np.array(chpSystem.chp.gen_e.get_memory())
+CHPstate = chp_gen_e > 0.
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=time, y=CHPstate,
+                         line={'color': 'rgba(100, 149, 237, 0.5)',
+                               'width': 1},
+                         name="CHP state")
+              )
+fig.update_layout(height=600, width=600,
+                  title_text="CHP operation")
+fig.update_xaxes(title_text="Time")
+fig.update_yaxes(title_text="On/Off")
+# %%
+
+plots.chargeState(chpSystem.storage, time)
+
+
+
+# %%
