11f9c1e553c3db40c9eac44241d4196477adb189
mherling <mherling@posteo.de>
2022-07-22 10:25:03 +0200

    Add wind component

--- SystemComponentsFast/src/components/wind.rs
+++ SystemComponentsFast/src/components/wind.rs
@@ -1,14 +1,16 @@
 // external
 use pyo3::prelude::*;
-use rand::Rng;
 
 use crate::misc::hist_memory;
 
 #[pyclass]
 #[derive(Clone)]
 pub struct Wind {
-    h: f32,  // height of hub
-    r: f32,  // radius (length) of blades
+    height: f32,  // height of hub
+    area: f32,  // effective rotor area
+    efficiency: f32, // simple effiency factor, TODO: curve
+    min_ws: f32, // minimum wind speed where electricity production starts
+    max_ws: f32, // maximum wind speed
     #[pyo3(get)]
     gen_e: Option<hist_memory::HistMemory>,
 }
@@ -21,11 +23,11 @@ impl Wind {
     /// * ?
     /// * hist (usize): Size of history memory (0 for no memory)
     #[new]
-    pub fn new(hist: usize) -> Self {
-        let mut rng = rand::thread_rng();
-
-        let a = rng.gen_range(0.8..=1.2) * coc * 1e3/eg * demand;
-
+    pub fn new(height: f32, radius: f32, min_ws: f32, max_ws: f32, 
+               efficiency: f32, hist: usize) -> Self {
+        
+        let pi = std::f32::consts::PI;
+        let area = pi / 2. * radius.powf(2.);
 
         let gen_e;
 
@@ -35,10 +37,14 @@ impl Wind {
             gen_e = None;
         }
 
-        let pv = PV {a: a,
-                     gen_e: gen_e,
-                    };
-        pv
+        let wind = Wind {height: height,
+                        area: area,
+                        efficiency: efficiency,
+                        min_ws: min_ws,
+                        max_ws: max_ws,
+                        gen_e: gen_e,
+                        };
+        wind
     }
 }
 
@@ -60,9 +66,24 @@ impl Wind {
     ///
     /// # Returns
     /// * f32: Resulting electrical power [W]
-    pub fn step(&mut self, eg: &f32) -> f32 {
+    pub fn step(&mut self, ws: &f32) -> f32 {
+        // ToDo: could be temperature dependent
+        let air_density = 1.2; // kg/m³
+        // ToDo: could be parameter
+        let z0 = 0.1; // [m] roughness length for agricultural land with a few buildings and 8 m high hedges seperated by approx. 500 m
+        // https://wind-data.ch/tools/profile.php?h=10&v=5&z0=0.1&abfrage=Refresh
+        let ws_hub = ws * (self.height / z0).ln() / (10. / z0).ln();
+
         // calculate electrical power generated
-        let power_e = ;
+        // https://rechneronline.de/wind-power/
+        let power_e: f32;
+
+        if ws_hub < self.min_ws || ws_hub > self.max_ws {
+            power_e = 0.;
+        }
+        else {
+            power_e = self.area * air_density * ws_hub.powf(3.) * self.efficiency;
+        }
 
         // save data
         self.save_hist(&power_e);
